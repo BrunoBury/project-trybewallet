8dd87155418540e38ea30cc6ac4be16230308cab
Bruno <bruno_bury@hotmail.com>
2023-06-19 19:46:32 -0300

    expense table created, req 06

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import '../styles/Table.css';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <div>
+        <h2>Gastos</h2>
+        <table className="custom-headers">
+          <thead>
+            <tr>
+              <th>Descrição</th>
+              <th>Tag</th>
+              <th>Método de pagamento</th>
+              <th>Valor</th>
+              <th>Moeda</th>
+              <th>Câmbio utilizado</th>
+              <th>Valor convertido</th>
+              <th>Moeda de conversão</th>
+              <th>Editar/Excluir</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
 import '../styles/Wallet.css';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -12,6 +13,9 @@ class Wallet extends React.Component {
           <div className="form-container">
             <WalletForm />
           </div>
+        </div>
+        <div>
+          <Table />
         </div>
       </div>
     );
